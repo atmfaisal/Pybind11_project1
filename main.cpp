2e41116bc34a75ce70f9042092496b977347771f
atmfaisal <faisal2408rcc@gmail.com>
2022-04-07 12:36:21 +0600

    First Commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <pybind11/pybind11.h>
+
+int summation(int var1, int var2) {
+    return var1 + var2;
+}
+
+PYBIND11_MODULE(test, handle) {
+handle.doc() = "Example of docstring!";
+handle.def("summation", &summation);
---
+++ test.py
@@ -0,0 +1,5 @@
+import build.test
+from build.test import *
+
+print("I am here")
+print("Sum: ", summation(2,3))
