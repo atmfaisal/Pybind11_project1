8d36ef8ad1dfa86ff5e05374b3521683ca7be844
atmfaisal <faisal2408rcc@gmail.com>
2022-04-07 13:29:26 +0600

    Prime Check Function Added!

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 #include <pybind11/pybind11.h>
 
+// First Summation Function Test
 int summation(int var1, int var2) {
     return var1 + var2;
 }
 
+
+// Prime Check
+bool prime_check(int var)
+{
+    bool is_prime = true;
+
+    if(var == 0 || var == 1) {
+        is_prime = false;
+    }
+
+    for(int i = 2; i <= var / 2; ++i) {
+        if( var % i) {
+            is_prime = false;
+            break;
+        }
+    }
+ return is_prime;
+}
+
+
 PYBIND11_MODULE(test, handle) {
 handle.doc() = "Example of docstring!";
 handle.def("summation", &summation);
+handle.def("prime_check", &prime_check);
 }
